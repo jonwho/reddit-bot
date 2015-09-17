0092636507ba95d4fb58ae63b5f5b3f64c3ccb04
Jon Ho <lj080805@gmail.com>
2015-09-17 12:58:35 -0700

    tested some stuff out

--- deletelinkbot.py
+++ deletelinkbot.py
@@ -11,12 +11,8 @@ username = f.readline().split('=')[-1].rstrip()
 password = f.readline().split('=')[-1].rstrip()
 f.close()
 
-# Prompts you for username and password in terminal
+# Login with values from file
 r.login(username = username, password = password)
-
-# Retrieve link you want to check against
-check_link = raw_input("Enter link you want to check: ")
-print 'Will check for duplicates of this link: ' + check_link
 
 # Use exact string matching first
 # If comment contains the string, delete comment?
@@ -51,6 +47,15 @@ print 'Will check for duplicates of this link: ' + check_link
 # don't care about that for now, do the simple case of building
 # a set and checking against the set
 
+# Some more steps
+# Go into subreddit
+# Get submissions and write submission.id into file if:
+#   submitted by AutoModerator
+#   thread title contains referral
+
+# Task: Open file to write and only write to file if thread passes criteria
+# f = open('filename', 'w') or something like that
+
 def run_bot():
   print("Grabbing subreddit...")
   subreddit = r.get_subreddit("myezpzbottester")
@@ -61,7 +66,41 @@ def run_bot():
     # decide on a strategy for checking duplicates
   print("Comments loop finished, time to sleep")
 
-while True:
-  run_bot()
-  # Run bot every 10 seconds
-  time.sleep(10)
+def get_user():
+  user = 'AutoModerator'
+  redditor = r.get_redditor(user)
+  print redditor
+  #print_attrs(redditor)
+  # print_attrs(redditor.get_submitted())
+  submissions = redditor.get_submitted()
+  submission = submissions.next()
+  print submission
+  print_attrs(submission)
+
+def get_comments(submission):
+  return submission.comments
+
+# Iterate over comments
+# for comment in comments:
+#   print comment
+
+# Method to print the methods available to an instance of a class
+def print_methods(instance = None):
+  if instance:
+    methods = [method for method in dir(instance) if callable(getattr(instance, method))]
+    for method in methods:
+      print method
+
+def print_fields(instance = None):
+  if instance:
+    fields = [f for f in dir(instance) if not callable(getattr(instance, f)) and not f.startswith('__')]
+    for f in fields:
+      print f
+
+# while True:
+  # run_bot()
+  # # Run bot every 10 seconds
+  # time.sleep(10)
+
+get_user()
+
